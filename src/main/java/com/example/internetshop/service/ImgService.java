a111503de27ba170a19a74f1052c1d96f8185103
OVERLORD <levchienko1998@gmail.com>
2023-11-30 03:41:59 +0200

    test adding img to db

--- src/main/java/com/example/internetshop/service/ImgService.java
+++ src/main/java/com/example/internetshop/service/ImgService.java
@@ -8,7 +8,6 @@ import org.springframework.stereotype.Service;
 import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.sql.SQLException;
 
 @Service
 public class ImgService {
