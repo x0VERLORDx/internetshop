517b991c5dda388c2f26eda3b2a94d957999e29d
savchiik <savchiik@gmail.com>
2023-12-01 04:47:55 +0400

    01.12.23 - пробный запрос

--- src/App.js
+++ src/App.js
@@ -2,6 +2,15 @@ import Header from './components/header/Header.jsx';
 import Promo from './components/promo/Proomo.jsx';
 import Newcollection from './components/newcollection/Newcollection.jsx';
 import Advantages from './components/advantages/Advantages.jsx';
+import NewDataCards from './services/NewDataCards.js';
+
+
+
+//создаем экземпляр класса 
+
+const newDataCards = new NewDataCards();
+
+newDataCards.getAllCards().then(res => res.data.results.forEach(item => console.log(item.name))); /* console.log(res) *///Берем массив всех карточек, и выводим только название товара
 
 function App() {
     return (
