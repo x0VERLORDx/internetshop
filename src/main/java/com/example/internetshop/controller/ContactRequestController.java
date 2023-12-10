e39493ceef0073a7c8b31a2cc917486fc743115b
OVERLORD <levchienko1998@gmail.com>
2023-12-10 06:24:28 +0200

    fix contactRequest

--- src/main/java/com/example/internetshop/controller/ContactRequestController.java
+++ src/main/java/com/example/internetshop/controller/ContactRequestController.java
@@ -4,19 +4,18 @@ import com.example.internetshop.dto.ContactRequestDto;
 import com.example.internetshop.service.ContactRequestService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/contact")
 public class ContactRequestController {
     @Autowired
     private ContactRequestService contactRequestService;
-
+    @CrossOrigin
     @PostMapping()
-    public void contactRequest(@RequestBody @Valid ContactRequestDto contactRequestDto){
+    public ResponseEntity contactRequest(@RequestBody @Valid ContactRequestDto contactRequestDto){
         contactRequestService.addRequest(contactRequestDto);
+        return ResponseEntity.ok().build();
     }
 }
