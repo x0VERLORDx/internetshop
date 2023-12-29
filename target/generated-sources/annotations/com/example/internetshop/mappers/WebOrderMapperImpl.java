d185e195e662d8315590ef14774ca9b045a65314
OVERLORD <levchienko1998@gmail.com>
2023-12-29 21:07:58 +0200

    remove release

--- target/generated-sources/annotations/com/example/internetshop/mappers/WebOrderMapperImpl.java
+++ target/generated-sources/annotations/com/example/internetshop/mappers/WebOrderMapperImpl.java
@@ -17,7 +17,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2023-12-29T21:02:52+0200",
+    date = "2023-12-29T21:06:44+0200",
     comments = "version: 1.4.2.Final, compiler: javac, environment: Java 21.0.1 (Oracle Corporation)"
 )
 @Component
